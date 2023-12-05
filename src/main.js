c6397e531567782bc92db1574064129d6ad70013
gunelaliyevaa <gunelaliyevaa01@gmail.com>
2023-12-05 21:39:30 +0400

    Comments added and optimizations made

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-// Constants and Variables
 const apiUrl = 'https://dummyjson.com/products';
 const itemsPerPage = 10;
 
@@ -7,7 +6,6 @@ let totalItemsFetched = 0;
 let selectedCategory = '';
 let currentFilteredPage = 1;
 
-// DOM Elements
 const productContainer = document.querySelector('.product-container');
 const currentPageDisplay = document.getElementById('current-page');
 const categoryDropdown = document.getElementById('categories');
@@ -18,7 +16,6 @@ const prevPageButton = document.getElementById('prev-page');
 const nextPageButton = document.getElementById('next-page');
 const filterButton = document.querySelector('input[type="submit"]');
 
-// Event Listeners
 prevPageButton.addEventListener('click', handlePrevPage);
 nextPageButton.addEventListener('click', handleNextPage);
 searchButton.addEventListener('click', handleSearch);
@@ -26,10 +23,10 @@ filterButton.addEventListener('click', handleFilter);
 resetButton.addEventListener('click', handleReset);
 
 
-// Initial Data Fetch
 fetchData();
 
-// Functions
+
+// Fetch from dummyjson
 function fetchData() {
     fetchAndDisplayData(`${apiUrl}?skip=${calculateSkip(currentPage)}&limit=${itemsPerPage}`);
 }
@@ -41,22 +38,11 @@ function fetchAndDisplayData(url) {
         .catch(handleError);
 }
 
-function calculateSkip(page) {
-    return (page - 1) * itemsPerPage;
-}
-
-function handleResponse(response) {
-    if (!response.ok) {
-        throw new Error(`Network response was not OK, status code: ${response.status}`);
-    }
-    return response.json();
-}
-
 function displayProducts(data) {
     console.log('Fetched data:', data);
-    totalItemsFetched += data.products.length;
+    totalItemsFetched += data.products.length; //incremented by the length of the data.products array
 
-    productContainer.innerHTML = '';
+    productContainer.innerHTML = '';  //clear current content
 
     if (isValidData(data)) {
         data.products.forEach(product => {
@@ -70,31 +56,41 @@ function displayProducts(data) {
     }
 }
 
-function isValidData(data) {
-    return data && data.products && Array.isArray(data.products);
-}
-
 function createProductElement(product) {
+    const {
+        id,
+        thumbnail,
+        title,
+        category,
+        price,
+        discountPercentage,
+        stock
+    } = product;
+
     const productDiv = document.createElement('div');
     productDiv.classList.add('product');
 
     productDiv.innerHTML = `
-        <a href="../product-info.html?id=${product.id}">
+        <a href="product-info.html?id=${id}">
             <div class="thumbnail-container">
-                <img src="${product.thumbnail}" alt="${product.title}" class="thumbnail">
+                <img src="${thumbnail}" alt="${title}" class="thumbnail">
             </div>
             <div class="product-details">
-                <h2>${product.title}</h2>
-                <h4>${product.category}</h4>
+                <h2>${title}</h2>
+                <h4>${category}</h4>
                 <div class="price-discount">
-                    <p class="price">$${product.price.toFixed(2)}</p>
-                    <p class="discount">${product.discountPercentage}% off</p>
+                    <p class="price">$${price.toFixed(2)}</p>
+                    <p class="discount">${discountPercentage}% off</p>
                 </div>
-                <p class="stock">Stock: ${product.stock} available</p>
+                <p class="stock">Stock: ${stock} available</p>
             </div>
         </a>
     `;
     return productDiv;
+}
+
+function calculateSkip(page) {
+    return (page - 1) * itemsPerPage;
 }
 
 function updatePaginationDisplay() {
@@ -106,6 +102,17 @@ function handleError(error) {
     alert(`Error: ${error.message}`);
 }
 
+function handleResponse(response) {
+    if (!response.ok) {
+        throw new Error(`Network response was not OK, status code: ${response.status}`);
+    }
+    return response.json();
+}
+
+function isValidData(data) {
+    return data && data.products && Array.isArray(data.products);
+}
+
 // Event Handlers
 function handlePrevPage() {
     if (currentPage > 1) {
@@ -115,7 +122,7 @@ function handlePrevPage() {
 }
 
 function handleNextPage() {
-    if (currentPage < Math.floor(totalItemsFetched / itemsPerPage)) currentPage++;
+    currentPage++;
     fetchData();
 }
 
@@ -138,6 +145,7 @@ function handleFilter() {
     currentPage = 1;
     selectedCategory = categoryDropdown.value;
     currentFilteredPage = 1; // Reset current page when applying a filter
+
     fetchAndDisplayData(`${apiUrl}/category/${selectedCategory}?skip=${calculateSkip(currentFilteredPage)}&limit=${itemsPerPage}`);
 }
 
